954b353d4b9bdc0d1042f2e86bc158671b933034
Manish Kumar <manishegley@gmail.com>
2022-03-11 16:30:51 +0530

    Subscribe user added

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+    //session_start();
+    //$email =$_SESSION['username'];
+    $db = mysqli_connect('localhost', 'root', 'Singh#640', 'u514565212_academy');
+    
+
+?>
+
+
+
+
+
+
+
+
+
+
