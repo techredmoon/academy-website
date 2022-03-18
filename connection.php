d60ddd84cba5717d02cbcfbb23fc52046b7c9f32
Manish Kumar <manishegley@gmail.com>
2022-03-18 13:43:55 +0530

    subscribe php testing

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+    $hostname = "localhost";
+    $username = "u514565212_alpha";
+    $password = "Singh#640";
+    $database = "u514565212_ztesting";
+    $db = mysqli_connect($hostname, $username, $password, $database);
+    //$db = mysqli_connect('localhost', 'root', '', 'barber');
+    
+
+?>
+
+
+
+
+
+
+
+
+
+
